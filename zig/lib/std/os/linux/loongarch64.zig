edb3102393dca6aaf8422e804481673676b19dd2
YANG Xudong <yangxudong@ymatrix.cn>
2024-01-30 13:18:08 +0800

    Fix register.

--- zig/lib/std/os/linux/loongarch64.zig
+++ zig/lib/std/os/linux/loongarch64.zig
@@ -13,64 +13,76 @@ const sigset_t = linux.sigset_t;
 const timespec = std.os.linux.timespec;
 
 pub fn syscall0(number: SYS) usize {
-    return asm volatile ("syscall 0\n\t"
-        : [ret] "={$a0}" (-> usize),
-        : [number] "{$a7}" (@intFromEnum(number)),
+    return asm volatile (
+        \\ syscall 0
+        \\
+        : [ret] "=$a0" (-> usize),
+        : [number] "$a7" (@intFromEnum(number)),
         : "$t0", "$t1", "$t2", "$t3", "$t4", "$t5", "$t6", "$t7", "$t8", "memory"
     );
 }
 
 pub fn syscall1(number: SYS, arg1: usize) usize {
-    return asm volatile ("syscall 0\n\t"
-        : [ret] "={$a0}" (-> usize),
-        : [number] "{$a7}" (@intFromEnum(number)),
-          [arg1] "{$a0}" (arg1),
+    return asm volatile (
+        \\ syscall 0
+        \\
+        : [ret] "=$a0" (-> usize),
+        : [number] "$a7" (@intFromEnum(number)),
+          [arg1] "$a0" (arg1),
         : "$t0", "$t1", "$t2", "$t3", "$t4", "$t5", "$t6", "$t7", "$t8", "memory"
     );
 }
 
 pub fn syscall2(number: SYS, arg1: usize, arg2: usize) usize {
-    return asm volatile ("syscall 0\n\t"
-        : [ret] "={$a0}" (-> usize),
-        : [number] "{$a7}" (@intFromEnum(number)),
-          [arg1] "{$a0}" (arg1),
-          [arg2] "{$a1}" (arg2),
+    return asm volatile (
+        \\ syscall 0
+        \\
+        : [ret] "=$a0" (-> usize),
+        : [number] "$a7" (@intFromEnum(number)),
+          [arg1] "$a0" (arg1),
+          [arg2] "$a1" (arg2),
         : "$t0", "$t1", "$t2", "$t3", "$t4", "$t5", "$t6", "$t7", "$t8", "memory"
     );
 }
 
 pub fn syscall3(number: SYS, arg1: usize, arg2: usize, arg3: usize) usize {
-    return asm volatile ("syscall 0\n\t"
-        : [ret] "={$a0}" (-> usize),
-        : [number] "{$a7}" (@intFromEnum(number)),
-          [arg1] "{$a0}" (arg1),
-          [arg2] "{$a1}" (arg2),
-          [arg3] "{$a2}" (arg3),
+    return asm volatile (
+        \\ syscall 0
+        \\
+        : [ret] "=$a0" (-> usize),
+        : [number] "$a7" (@intFromEnum(number)),
+          [arg1] "$a0" (arg1),
+          [arg2] "$a1" (arg2),
+          [arg3] "$a2" (arg3),
         : "$t0", "$t1", "$t2", "$t3", "$t4", "$t5", "$t6", "$t7", "$t8", "memory"
     );
 }
 
 pub fn syscall4(number: SYS, arg1: usize, arg2: usize, arg3: usize, arg4: usize) usize {
-    return asm volatile ("syscall 0\n\t"
-        : [ret] "={$a0}" (-> usize),
-        : [number] "{$a7}" (@intFromEnum(number)),
-          [arg1] "{$a0}" (arg1),
-          [arg2] "{$a1}" (arg2),
-          [arg3] "{$a2}" (arg3),
-          [arg4] "{$a3}" (arg4),
+    return asm volatile (
+        \\ syscall 0
+        \\
+        : [ret] "=$a0" (-> usize),
+        : [number] "$a7" (@intFromEnum(number)),
+          [arg1] "$a0" (arg1),
+          [arg2] "$a1" (arg2),
+          [arg3] "$a2" (arg3),
+          [arg4] "$a3" (arg4),
         : "$t0", "$t1", "$t2", "$t3", "$t4", "$t5", "$t6", "$t7", "$t8", "memory"
     );
 }
 
 pub fn syscall5(number: SYS, arg1: usize, arg2: usize, arg3: usize, arg4: usize, arg5: usize) usize {
-    return asm volatile ("syscall 0\n\t"
-        : [ret] "={$a0}" (-> usize),
-        : [number] "{$a7}" (@intFromEnum(number)),
-          [arg1] "{$a0}" (arg1),
-          [arg2] "{$a1}" (arg2),
-          [arg3] "{$a2}" (arg3),
-          [arg4] "{$a3}" (arg4),
-          [arg5] "{$a4}" (arg5),
+    return asm volatile (
+        \\ syscall 0
+        \\
+        : [ret] "=$a0" (-> usize),
+        : [number] "$a7" (@intFromEnum(number)),
+          [arg1] "$a0" (arg1),
+          [arg2] "$a1" (arg2),
+          [arg3] "$a2" (arg3),
+          [arg4] "$a3" (arg4),
+          [arg5] "$a4" (arg5),
         : "$t0", "$t1", "$t2", "$t3", "$t4", "$t5", "$t6", "$t7", "$t8", "memory"
     );
 }
@@ -84,15 +96,17 @@ pub fn syscall6(
     arg5: usize,
     arg6: usize,
 ) usize {
-    return asm volatile ("syscall 0\n\t"
-        : [ret] "={$a0}" (-> usize),
-        : [number] "{$a7}" (@intFromEnum(number)),
-          [arg1] "{$a0}" (arg1),
-          [arg2] "{$a1}" (arg2),
-          [arg3] "{$a2}" (arg3),
-          [arg4] "{$a3}" (arg4),
-          [arg5] "{$a4}" (arg5),
-          [arg6] "{$a5}" (arg6),
+    return asm volatile (
+        \\ syscall 0
+        \\
+        : [ret] "=$a0" (-> usize),
+        : [number] "$a7" (@intFromEnum(number)),
+          [arg1] "$a0" (arg1),
+          [arg2] "$a1" (arg2),
+          [arg3] "$a2" (arg3),
+          [arg4] "$a3" (arg4),
+          [arg5] "$a4" (arg5),
+          [arg6] "$a5" (arg6),
         : "$t0", "$t1", "$t2", "$t3", "$t4", "$t5", "$t6", "$t7", "$t8", "memory"
     );
 }
@@ -107,16 +121,18 @@ pub fn syscall7(
     arg6: usize,
     arg7: usize,
 ) usize {
-    return asm volatile ("syscall 0\n\t"
-        : [ret] "={$a0}" (-> usize),
-        : [number] "{$a7}" (@intFromEnum(number)),
-          [arg1] "{$a0}" (arg1),
-          [arg2] "{$a1}" (arg2),
-          [arg3] "{$a2}" (arg3),
-          [arg4] "{$a3}" (arg4),
-          [arg5] "{$a4}" (arg5),
-          [arg6] "{$a5}" (arg6),
-          [arg7] "{$a6}" (arg7),
+    return asm volatile (
+        \\ syscall 0
+        \\
+        : [ret] "=$a0" (-> usize),
+        : [number] "$a7" (@intFromEnum(number)),
+          [arg1] "$a0" (arg1),
+          [arg2] "$a1" (arg2),
+          [arg3] "$a2" (arg3),
+          [arg4] "$a3" (arg4),
+          [arg5] "$a4" (arg5),
+          [arg6] "$a5" (arg6),
+          [arg7] "$a6" (arg7),
         : "$t0", "$t1", "$t2", "$t3", "$t4", "$t5", "$t6", "$t7", "$t8", "memory"
     );
 }
@@ -129,9 +145,10 @@ pub const restore = restore_rt;
 
 pub fn restore_rt() callconv(.Naked) noreturn {
     asm volatile (
-        \\ syscall      0
+        \\ syscall 0
+        \\
         :
-        : [number] "{$a7}" (@intFromEnum(SYS.rt_sigreturn)),
+        : [number] "$a7" (@intFromEnum(SYS.rt_sigreturn)),
         : "$t0", "$t1", "$t2", "$t3", "$t4", "$t5", "$t6", "$t7", "$t8", "memory"
     );
 }
