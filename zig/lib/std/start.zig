db840b5cb7e6c01b4493a7a5eb979a08b0caad66
YANG Xudong <yangxudong@ymatrix.cn>
2024-01-31 14:06:46 +0800

    Fix start.

--- zig/lib/std/start.zig
+++ zig/lib/std/start.zig
@@ -333,10 +333,9 @@ fn _start() callconv(.Naked) noreturn {
             \\  stx %%l0, %[argc_argv_ptr]
             ,
             .loongarch64 =>
-            \\ ori      $fp, $zero, 0
-            \\ st.d     $sp, %[argc_argv_ptr]
-            \\ addi.d   $t8, $zero, -16
-            \\ and      $sp, $sp, $t8
+            \\ or           $fp, $zero, $zero
+            \\ st.d         $sp, %[argc_argv_ptr]
+            \\ bstrins.d    $sp, $zero, 3, 0
             \\ b %[posixCallMainAndExit]
             ,
             else => @compileError("unsupported arch"),
