9e5e68457ba62b50fb5c72b92102e660413463ac
YANG Xudong <yangxudong@ymatrix.cn>
2024-01-31 10:20:48 +0800

    Fix error.

--- zig/lib/std/start.zig
+++ zig/lib/std/start.zig
@@ -335,11 +335,11 @@ fn _start() callconv(.Naked) noreturn {
             .loongarch64 =>
             \\ ori  $fp, $zero, 0
             \\ st.d $sp, %[argc_argv_ptr]
-            \\ .set push
-            \\ .set noat
+            \\ .set push;
+            \\ .set noat;
             \\ addi.d $1, $zero, -16
             \\ and $sp, $sp, $1
-            \\ .set pop
+            \\ .set pop;
             \\ b %[posixCallMainAndExit]
             ,
             else => @compileError("unsupported arch"),
