743d81d1545e662fb26a82bf2687a97e15d15675
YANG Xudong <yangxudong@ymatrix.cn>
2024-02-05 14:00:30 +0800

    Revert "Unset mold."
    This reverts commit 6bb83ad3b3e8b2d3cffdaa9f2511938f46e3970b.

--- zig/src/link/Elf.zig
+++ zig/src/link/Elf.zig
@@ -2538,13 +2538,20 @@ fn linkWithLLD(self: *Elf, arena: Allocator, prog_node: *std.Progress.Node) !voi
         // We will invoke ourselves as a child process to gain access to LLD.
         // This is necessary because LLD does not behave properly as a library -
         // it calls exit() and does not reset all global data between invocations.
-        const linker_command = "ld.lld";
+        const linker_command = switch (target.cpu.arch) {
+            std.Target.Cpu.Arch.loongarch64 => "mold",
+            else => "ld.lld",
+        };
+        const use_mold = mem.eql(u8, linker_command, "mold");
+
         try argv.appendSlice(&[_][]const u8{ comp.self_exe_path.?, linker_command });
         if (is_obj) {
             try argv.append("-r");
         }
 
-        try argv.append("--error-limit=0");
+        if (!use_mold) {
+            try argv.append("--error-limit=0");
+        }
 
         if (comp.sysroot) |sysroot| {
             try argv.append(try std.fmt.allocPrint(arena, "--sysroot={s}", .{sysroot}));
@@ -2945,7 +2952,8 @@ fn linkWithLLD(self: *Elf, arena: Allocator, prog_node: *std.Progress.Node) !voi
             // behave properly as a library, unfortunately.
             // https://github.com/ziglang/zig/issues/3825
             var child = std.ChildProcess.init(argv.items, arena);
-            if (comp.clang_passthrough_mode) {
+
+            if (comp.clang_passthrough_mode or use_mold) {
                 child.stdin_behavior = .Inherit;
                 child.stdout_behavior = .Inherit;
                 child.stderr_behavior = .Inherit;
